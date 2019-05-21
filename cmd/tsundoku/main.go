24c862854ba3e6af22d269c965e11ff469d19d86
moboa <mbajifope@gmail.com>
2019-05-21 00:42:48 -0400

    Implement url hostname parsing

--- cmd/tsundoku/main.go
+++ cmd/tsundoku/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/url"
 	"os"
 
 	"github.com/urfave/cli"
@@ -12,8 +13,19 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "tsundoku"
 	app.Usage = "manga downloader"
+	app.UsageText = app.Name + " [--help] [--version] [<url>]"
 	app.Action = func(c *cli.Context) error {
-		fmt.Printf("Hello %q", c.Args().Get(0))
+		if c.NArg() != 1 {
+			errorMsg := "Incorrect number of arguments. Please run " + app.Name + " -h."
+			return cli.NewExitError(errorMsg, 1)
+		}
+
+		chapterURL, err := url.Parse(c.Args().Get(0))
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(chapterURL.Hostname())
 		return nil
 	}
 
